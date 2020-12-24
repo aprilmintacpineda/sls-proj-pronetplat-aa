830648c32f31573e35f1266acdf167a0bb7afc66
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-12-24 19:11:08 +0800

    fixed call to user.fetchByEmail

--- src/functions/guest/forgotPasswordSend.js
+++ src/functions/guest/forgotPasswordSend.js
@@ -11,7 +11,7 @@ module.exports.handler = async ({ body }) => {
 
   try {
     const user = new User();
-    await User.fetchByEmail(body.email);
+    await user.fetchByEmail(body.email);
     await user.forgotPassword();
   } catch (error) {
     console.log(error);
