07129b9f358961397e66a404b64f7924adaed135
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-12-25 17:12:15 +0800

    verify email

--- src/functions/auth/verifyEmail.js
+++ src/functions/auth/verifyEmail.js
@@ -1,5 +1,7 @@
+const { query } = require('faunadb');
+
 const validate = require('/opt/nodejs/utils/validate');
-const { parseAuth } = require('/opt/nodejs/utils/helpers');
+const { parseAuth, verifyHash, hasTimePassed } = require('/opt/nodejs/utils/helpers');
 const jwt = require('/opt/nodejs/utils/jwt');
 const User = require('/opt/nodejs/models/User');
 
@@ -15,8 +17,28 @@ module.exports.handler = async ({ headers, body }) => {
     const auth = await jwt.verify(parseAuth(headers));
     const user = new User();
     await user.getById(auth.data.id);
-    console.log(auth);
-    console.log(user);
+
+    if (hasTimePassed(user.data.emailConfirmCodeExpiresAt)) return { statusCode: 410 };
+
+    if (
+      !await verifyHash(
+        formBody.verificationCode,
+        user.data.hashedEmailVerificationCode
+      )
+    )
+      throw new Error('Incorrect verification code');
+
+    await user.update({ emailVerifiedAt: query.Now() });
+    const authUser = user.toResponseData();
+    const authToken = await jwt.sign(authUser);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        authUser,
+        authToken
+      })
+    };
   } catch (error) {
     console.log('error', error);
   }
