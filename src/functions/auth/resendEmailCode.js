1f5be5edcaaf68dfc99510fff0a4536ab178f723
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-12-26 11:25:56 +0800

    resend email code;

---
+++ src/functions/auth/resendEmailCode.js
@@ -0,0 +1,48 @@
+const {
+  parseAuth,
+  hasTimePassed,
+  randomCode,
+  hash
+} = require('/opt/nodejs/utils/helpers');
+const jwt = require('/opt/nodejs/utils/jwt');
+const { getTimeOffset } = require('/opt/nodejs/utils/faunadb');
+const { sendEmailVerificationCode } = require('/opt/nodejs/utils/sendEmail');
+
+const User = require('/opt/nodejs/models/User');
+
+module.exports.handler = async ({ headers }) => {
+  try {
+    const auth = await jwt.verify(parseAuth(headers));
+    const user = new User();
+    await user.fetchById(auth.data.id);
+
+    if (!hasTimePassed(user.data.emailCodeCanSendAt)) return { statusCode: 429 };
+
+    const emailVerificationCode = randomCode();
+    const hashedEmailVerificationCode = await hash(emailVerificationCode);
+    const timeOffset = getTimeOffset();
+
+    await user.update({
+      hashedEmailVerificationCode,
+      emailCodeCanSendAt: timeOffset,
+      emailConfirmCodeExpiresAt: timeOffset
+    });
+
+    await sendEmailVerificationCode({
+      recipient: user.data.email,
+      emailVerificationCode,
+      isResend: true
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        emailCodeCanSendAt: user.data.emailCodeCanSendAt
+      })
+    };
+  } catch (error) {
+    console.log('error', error);
+  }
+
+  return { statusCode: 403 };
+};
