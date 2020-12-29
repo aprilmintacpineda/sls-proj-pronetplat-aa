5d1a248d843996d2e24226f5b9fc3642f98153fb
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-12-29 12:27:22 +0800

    used RequestParameters

--- src/dependencies/nodejs/utils/helpers.js
+++ src/dependencies/nodejs/utils/helpers.js
@@ -3,9 +3,8 @@ const bcrypt = require('bcrypt');
 module.exports.randomCode = () => Math.random().toString(32).substr(2);
 module.exports.hash = value => bcrypt.hash(value, 10);
 
-module.exports.parseAuth = ({ Authorization, authorization }) => {
-  const authToken = Authorization || authorization || '';
-  return authToken.replace(/bearer/gim, '').trim();
+module.exports.parseAuth = ({ Authorization }) => {
+  return Authorization.replace(/bearer/gim, '').trim();
 };
 
 module.exports.verifyHash = (plainValue, hashedValue) => {
