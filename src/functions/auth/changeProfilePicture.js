574f242adfc5bd04c7ce7ccebeccbfd586636465
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-12-29 09:42:47 +0800

    used getSignedUrl

--- src/functions/auth/changeProfilePicture.js
+++ src/functions/auth/changeProfilePicture.js
@@ -2,24 +2,33 @@ const AWS = require('aws-sdk');
 
 const jwt = require('/opt/nodejs/utils/jwt');
 const { parseAuth } = require('/opt/nodejs/utils/helpers');
+const validate = require('/opt/nodejs/utils/validate');
 const User = require('/opt/nodejs/models/User');
 
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-module.exports.handler = async ({ headers }) => {
+function hasErrors ({ mimeType }) {
+  return validate(mimeType, ['required', 'options:image/jpeg,image/png']);
+}
+
+module.exports.handler = async ({ headers, body }) => {
   try {
     const auth = await jwt.verify(parseAuth(headers));
     const user = new User();
     await user.getById(auth.data.id);
+    const formBody = JSON.parse(body);
+
+    if (hasErrors(formBody)) return '';
 
     const presignedUrl = await new Promise((resolve, reject) => {
-      s3.createPresignedPost(
+      s3.getSignedUrl(
+        'putObject',
         {
           Bucket: process.env.USERS_BUCKET,
-          Expires: 10,
-          Fields: {
-            key: `profilePictures/${user.data.id}`
-          }
+          Expires: 15,
+          ACL: 'public-read',
+          key: `profilePictures/${user.data.id}`,
+          ContentType: formBody.mimeType
         },
         (error, data) => {
           if (error) reject(error);
@@ -28,10 +37,12 @@ module.exports.handler = async ({ headers }) => {
       );
     });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(presignedUrl)
-    };
+    console.log(presignedUrl);
+
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify({})
+    // };
   } catch (error) {
     console.log('error', error);
   }
