b2809f4973a005b295e49d3dedfb19ee8d79d4f3
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-12-31 21:50:07 +0800

    debugging firebase admin

--- src/dependencies/nodejs/utils/firebase.js
+++ src/dependencies/nodejs/utils/firebase.js
@@ -2,16 +2,16 @@ const fs = require('fs').promises;
 const firebaseAdmin = require('firebase-admin');
 const path = require('path');
 
-const configPath = path.join(__dirname, '../resources/firebase_admin.json');
-
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(configPath)
-});
-
 module.exports.isValidDeviceToken = async deviceToken => {
+  const configPath = path.join(__dirname, '../resources/firebase_admin.json');
+
   const file = await fs.readFile(configPath, 'utf-8');
 
   console.log(file.toString());
+
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(configPath)
+  });
 
   const {
     results: [notifResult]
