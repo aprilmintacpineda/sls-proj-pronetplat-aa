bd872025867071b9766b24f1228d1e40f5cf5130
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-01-01 14:18:45 +0800

    fixed count

--- src/functions/auth/contactRequestCount.js
+++ src/functions/auth/contactRequestCount.js
@@ -1,16 +1,17 @@
 const NetworkRequest = require('/opt/nodejs/models/NetworkRequest');
+const jwt = require('/opt/nodejs/utils/jwt');
+const { parseAuth } = require('/opt/nodejs/utils/helpers');
 
-module.exports.handler = async () => {
+module.exports.handler = async ({ headers }) => {
   let count = 0;
 
   try {
+    const auth = await jwt.verify(parseAuth(headers));
     const networkRequest = new NetworkRequest();
-    count = await networkRequest.countReceivedRequests();
+    count = await networkRequest.countReceivedRequests(auth.data.id);
   } catch (error) {
     console.log(error);
   }
-
-  console.log(count);
 
   return {
     statusCode: 200,
