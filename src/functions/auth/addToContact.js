563c986696a68652ac08df4439e9c45bbf377096
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-01-02 13:36:11 +0800

    added duplicate checks;

--- src/functions/auth/addToContact.js
+++ src/functions/auth/addToContact.js
@@ -2,8 +2,8 @@ const jwt = require('/opt/nodejs/utils/jwt');
 const { parseAuth } = require('/opt/nodejs/utils/helpers');
 const { sendPushNotification } = require('/opt/nodejs/utils/notifications');
 const User = require('/opt/nodejs/models/User');
-// const ContactRequest = require('/opt/nodejs/models/ContactRequest');
-// const Contact = require('/opt/nodejs/models/Contact');
+const ContactRequest = require('/opt/nodejs/models/ContactRequest');
+const Contact = require('/opt/nodejs/models/Contact');
 
 module.exports.handler = async ({ pathParameters: { contactId }, headers }) => {
   try {
@@ -13,36 +13,36 @@ module.exports.handler = async ({ pathParameters: { contactId }, headers }) => {
 
     const user = new User();
     const targetUser = new User();
-    // const contact = new Contact();
-    // const contactRequest = new ContactRequest();
+    const contact = new Contact();
+    const contactRequest = new ContactRequest();
 
-    // if (
-    //   await contactRequest.hasPendingRequest({
-    //     from: contactId,
-    //     to: auth.data.id
-    //   }) ||
-    //   await contactRequest.hasPendingRequest({
-    //     from: auth.data.id,
-    //     to: contactId
-    //   })
-    // )
-    //   return { statusCode: 422 };
+    if (
+      await contactRequest.hasPendingRequest({
+        from: contactId,
+        to: auth.data.id
+      }) ||
+      await contactRequest.hasPendingRequest({
+        from: auth.data.id,
+        to: contactId
+      })
+    )
+      return { statusCode: 422 };
 
     await Promise.all([user.getById(auth.data.id), targetUser.getById(contactId)]);
 
     if (!targetUser.data.completedFirstSetupAt) throw new Error('Target user not setup.');
 
-    // await Promise.all([
-    //   contact.create({
-    //     ownerId: user.data.id,
-    //     contactId: targetUser.data.id,
-    //     status: 'pending'
-    //   }),
-    //   contactRequest.create({
-    //     senderId: user.data.id,
-    //     recipientId: targetUser.data.id
-    //   })
-    // ]);
+    await Promise.all([
+      contact.create({
+        ownerId: user.data.id,
+        contactId: targetUser.data.id,
+        status: 'pending'
+      }),
+      contactRequest.create({
+        senderId: user.data.id,
+        recipientId: targetUser.data.id
+      })
+    ]);
 
     const { profilePicture, firstName, middleName, surname } = user.data;
 
