bac33217218e3ac3ab8f0de9fdbcd70f02a6215c
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-01-03 23:02:00 +0800

    decline contact request

--- src/functions/auth/declineContactRequest.js
+++ src/functions/auth/declineContactRequest.js
@@ -1,10 +1,9 @@
-const { getAuthTokenFromHeaders } = require('/opt/nodejs/utils/helpers');
+const { getAuthTokeFromHeaders } = require('/opt/nodejs/utils/helpers');
 const jwt = require('/opt/nodejs/utils/jwt');
 const validate = require('/opt/nodejs/utils/validate');
 const { sendPushNotification } = require('/opt/nodejs/utils/notifications');
 
 const ContactRequest = require('/opt/nodejs/models/ContactRequest');
-const Contact = require('/opt/nodejs/models/Contact');
 const Notification = require('/opt/nodejs/models/Notification');
 
 function hasErrors ({ id }) {
@@ -14,37 +13,24 @@ function hasErrors ({ id }) {
 module.exports.handler = async ({ headers, body }) => {
   try {
     const {
-      data: { id, firstName, middleName, surname, profilePicture, bio, company, jobTitle }
-    } = await jwt.verify(getAuthTokenFromHeaders(headers));
+      data: { id, firstName, middleName, surname, profilePicture }
+    } = await jwt.verify(getAuthTokeFromHeaders(headers));
 
     const formBody = JSON.parse(body);
-    if (hasErrors(formBody)) throw new Error('invalid form body');
+    if (hasErrors(formBody)) throw new Error('invalid body');
 
     const contactRequest = new ContactRequest();
     await contactRequest.getById(formBody.id);
     if (contactRequest.data.recipientId !== id) throw new Error('user is not recipient.');
 
-    const { senderId } = contactRequest.data;
-    const contact = new Contact();
-
-    await Promise.all([
-      contact.create({
-        ownerId: id,
-        contactId: senderId
-      }),
-      contact.create({
-        ownerId: senderId,
-        contactId: id
-      })
-    ]);
-
     const notification = new Notification();
+    const { senderId } = contactRequest.data;
 
     await Promise.all([
-      await contactRequest.hardDelete(),
-      await notification.create({
+      contactRequest.hardDelete(),
+      notification.create({
         userId: senderId,
-        type: 'contactRequestAccepted',
+        type: 'contactRequestDeclined',
         message: '{fullname} has accepted your contact request.',
         actorId: id
       })
@@ -54,18 +40,14 @@ module.exports.handler = async ({ headers, body }) => {
 
     await sendPushNotification({
       userId: senderId,
-      title: 'Contact request accepted',
-      body: `${fullName} has accepted your contact request.`,
-      type: 'contactRequestAccepted',
+      title: 'Contact request declined',
+      body: `${fullName} has declined your contact request.`,
+      type: 'contactRequestDeclined',
       data: {
-        id,
         profilePicture,
         firstName,
         middleName,
-        surname,
-        bio,
-        company,
-        jobTitle
+        surname
       }
     });
 
