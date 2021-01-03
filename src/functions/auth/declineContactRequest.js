1e4e271aa2fe541c1b52435fef8719857db9d3a8
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-01-03 23:20:08 +0800

    fixed notification message;

--- src/functions/auth/declineContactRequest.js
+++ src/functions/auth/declineContactRequest.js
@@ -31,7 +31,7 @@ module.exports.handler = async ({ headers, body }) => {
       notification.create({
         userId: senderId,
         type: 'contactRequestDeclined',
-        message: '{fullname} has accepted your contact request.',
+        message: '{fullname} has declined your contact request.',
         actorId: id
       })
     ]);
