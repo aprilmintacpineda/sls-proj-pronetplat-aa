b974e946da8d3aebed34c5bd271ca26bdbb7496f
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-01-05 21:06:48 +0800

    badgeCount

--- src/dependencies/nodejs/models/ContactRequest.js
+++ src/dependencies/nodejs/models/ContactRequest.js
@@ -3,10 +3,6 @@ const Model = require('/opt/nodejs/classes/Model');
 module.exports = class ContactRequest extends Model {
   collection = 'contactRequests';
 
-  countReceivedRequests (recipientId) {
-    return this.countByIndex('contactRequestsByRecipient', recipientId);
-  }
-
   hasPendingRequest ({ from, to }) {
     return this.countByIndex('contactRequestByUserFromTo', from, to);
   }
---
+++ src/functions/auth/badgeCount.js
@@ -0,0 +1,46 @@
+const { query } = require('faunadb');
+const { initClient } = require('/opt/nodejs/utils/faunadb');
+const jwt = require('/opt/nodejs/utils/jwt');
+const { getAuthTokenFromHeaders } = require('/opt/nodejs/utils/helpers');
+
+const counts = [
+  {
+    name: 'receivedContactRequest',
+    index: 'contactRequestsByRecipient'
+  },
+  {
+    name: 'receivedContactRequest',
+    index: 'contactRequestsByRecipient'
+  }
+];
+
+module.exports.handler = async ({ headers }) => {
+  const count = 0;
+
+  try {
+    const {
+      data: { id }
+    } = await jwt.verify(getAuthTokenFromHeaders(headers));
+
+    const client = initClient();
+
+    const result = await client.query(
+      counts.reduce((accumulator, { name, index }) => {
+        accumulator[name] = query.Count(
+          query.Paginate(query.Match(query.Index(index), id))
+        );
+
+        return accumulator;
+      }, {})
+    );
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: count
+  };
+};
