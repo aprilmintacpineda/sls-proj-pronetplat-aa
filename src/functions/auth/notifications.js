71f62c35c6eadfd76f13a60d36398c8ea3ac9d42
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-01-09 13:27:40 +0800

    fixed typo

--- src/functions/auth/notifications.js
+++ src/functions/auth/notifications.js
@@ -30,7 +30,7 @@ module.exports.handler = async ({ headers, queryStringParameters }) => {
                 query.Get(query.Ref(query.Collection('users'), query.Var('actorId')))
               ),
               markedAsSeen: query.If(
-                query.isNull(query.Select(['seenAt'], query.Var('data'), null)),
+                query.IsNull(query.Select(['seenAt'], query.Var('data'), null)),
                 query.Update(query.Var('ref'), {
                   data: {
                     seenAt: query.Now()
