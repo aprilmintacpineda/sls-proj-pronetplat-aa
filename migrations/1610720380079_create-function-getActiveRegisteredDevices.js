8d21b9c9d1e06ab0f6ba2034327169f4b5cc2deb
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-01-15 23:35:47 +0800

    removed time handling on index; getActiveRegisteredDevices UDF;

---
+++ migrations/1610720380079_create-function-getActiveRegisteredDevices.js
@@ -0,0 +1,49 @@
+const name = 'getActiveRegisteredDevices';
+
+module.exports.up = q => {
+  return q.If(
+    q.Not(q.Exists(q.Function(name))),
+    q.CreateFunction({
+      name,
+      body: q.Query(
+        q.Lambda(
+          ['userId', 'nextToken'],
+          q.Filter(
+            q.Map(
+              q.Paginate(
+                q.Match(
+                  q.Index('registeredDeviceByUserIdDeviceToken'),
+                  q.Var('userId')
+                ),
+                {
+                  size: 20,
+                  after: q.If(
+                    q.Equals(q.Var('nextToken'), ''),
+                    0,
+                    q.Var('nextToken')
+                  )
+                }
+              ),
+              q.Lambda(['ref'], q.Get(q.Var('ref')))
+            ),
+            q.Lambda(
+              ['document'],
+              q.LT(
+                q.Now(),
+                q.Time(
+                  q.Select(['data', 'expiresAt'], q.Var('document'))
+                )
+              )
+            )
+          )
+        )
+      )
+    }),
+    null
+  );
+};
+
+module.exports.down = q => {
+  const func = q.Function(name);
+  return q.If(q.Exists(func), q.Delete(func), null);
+};
