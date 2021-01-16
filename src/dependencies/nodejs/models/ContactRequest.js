fae8a4f147f4bbc9af43d1a135c04457e8fcaa77
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-01-16 19:43:11 +0800

    removed Boolean;

--- src/dependencies/nodejs/models/ContactRequest.js
+++ src/dependencies/nodejs/models/ContactRequest.js
@@ -7,12 +7,10 @@ module.exports = class ContactRequest extends Model {
   }
 
   hasPendingRequest ({ senderId, recipientId }) {
-    return Boolean(
-      this.countByIndex(
-        'contactRequestBySenderIdRecipientId',
-        senderId,
-        recipientId
-      )
+    return this.countByIndex(
+      'contactRequestBySenderIdRecipientId',
+      senderId,
+      recipientId
     );
   }
 
