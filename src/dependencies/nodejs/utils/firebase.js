c97b7039863a28e4cad4222daf04855892a23117
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-01-17 17:19:31 +0800

    fixed mapping; log env vars

--- src/dependencies/nodejs/utils/firebase.js
+++ src/dependencies/nodejs/utils/firebase.js
@@ -34,6 +34,8 @@ module.exports.sendPushNotification = ({
   notification,
   data
 }) => {
+  console.log(process.env);
+
   if (tokens && tokens.length) {
     return firebaseAdmin.messaging().sendToDevice(
       tokens,
