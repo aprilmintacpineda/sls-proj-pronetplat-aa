931ebe378d10219a760eacb95a72a1e441010162
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-02-14 16:06:13 +0800

    removed console log

--- src/dependencies/nodejs/utils/jwt.js
+++ src/dependencies/nodejs/utils/jwt.js
@@ -15,12 +15,10 @@ const verifyAsync = promisify(jwt.verify);
 const secret = '__JWTSecret__';
 
 module.exports.sign = async data => {
-  console.log('secret', secret);
   return signAsync({ data }, secret, signConfig);
 };
 
 module.exports.verify = async token => {
-  console.log('secret', secret);
   return verifyAsync(token, secret, verifyConfig);
 };
 
