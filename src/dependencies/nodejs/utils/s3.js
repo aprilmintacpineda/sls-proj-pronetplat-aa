1af1b90bcc90dd2373d7d5b2f8c3a8c97dae2ff0
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-02-15 21:40:40 +0800

    revert to putObject

--- src/dependencies/nodejs/utils/s3.js
+++ src/dependencies/nodejs/utils/s3.js
@@ -1,11 +1,8 @@
 const AWS = require('aws-sdk');
-const mimetypes = require('mime-types');
-const { randomNum } = require('./helpers');
 
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
-  signatureVersion: 'v4',
-  useAccelerateEndpoint: true
+  signatureVersion: 'v4'
 });
 
 module.exports.getObjectPromise = params => {
@@ -31,42 +28,6 @@ module.exports.uploadPromise = params => {
     s3.upload(params, (error, data) => {
       if (error) reject(error);
       else resolve(data);
-    });
-  });
-};
-
-module.exports.profilePictureUploadUrlPromise = (userId, type) => {
-  return new Promise((resolve, reject) => {
-    const ext = mimetypes.extension(type);
-    const objectName = `${userId}_${randomNum()}.${ext}`;
-    const bucketName = process.env.usersBucket;
-    const key = `newProfilePicture_${objectName}`;
-
-    const params = {
-      Bucket: bucketName,
-      Expires: 300,
-      Fields: {
-        key,
-        acl: 'private'
-      },
-      Conditions: [
-        { bucket: bucketName },
-        ['content-length-range', 1024, 5242880],
-        { acl: 'private' },
-        { key },
-        ['starts-with', '$Content-Type', 'image/']
-      ]
-    };
-
-    s3.createPresignedPost(params, (error, data) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve({
-          data,
-          profilePicture: `https://${bucketName}.s3-ap-southeast-1.amazonaws.com/profilePicture_${objectName}`
-        });
-      }
     });
   });
 };
--- src/functions/auth/changeProfilePicture.js
+++ src/functions/auth/changeProfilePicture.js
@@ -1,10 +1,10 @@
+const mimetypes = require('mime-types');
 const {
+  randomNum,
   checkRequiredHeaderValues
 } = require('dependencies/nodejs/utils/helpers');
 const jwt = require('dependencies/nodejs/utils/jwt');
-const {
-  profilePictureUploadUrlPromise
-} = require('dependencies/nodejs/utils/s3');
+const { s3 } = require('dependencies/nodejs/utils/s3');
 const {
   throwIfNotCompletedSetup
 } = require('dependencies/nodejs/utils/users');
@@ -28,14 +28,23 @@ module.exports.handler = async ({ headers, body }) => {
 
     throwIfNotCompletedSetup(authUser);
 
-    const uploadUrls = await profilePictureUploadUrlPromise(
-      authUser.id,
-      formBody.type
-    );
+    const ext = mimetypes.extension(formBody.type);
+    const objectName = `${authUser.id}_${randomNum()}.${ext}`;
+
+    const signedUrl = await s3.getSignedUrlPromise('putObject', {
+      Bucket: process.env.usersBucket,
+      Expires: 10,
+      ACL: 'private',
+      Key: `newProfilePicture_${objectName}`,
+      ContentType: formBody.type
+    });
 
     return {
       statusCode: 200,
-      body: JSON.stringify(uploadUrls)
+      body: JSON.stringify({
+        signedUrl,
+        profilePicture: `https://${process.env.usersBucket}.s3-ap-southeast-1.amazonaws.com/profilePicture_${objectName}`
+      })
     };
   } catch (error) {
     console.log('error', error);
