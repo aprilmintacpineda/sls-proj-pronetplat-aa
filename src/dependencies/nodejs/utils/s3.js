b58bfeabac3c97d6574ee8da5db4d07cb2663a3d
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-02-15 21:51:11 +0800

    used useAccelerateEndpoint s3

--- src/dependencies/nodejs/utils/s3.js
+++ src/dependencies/nodejs/utils/s3.js
@@ -2,7 +2,8 @@ const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
-  signatureVersion: 'v4'
+  signatureVersion: 'v4',
+  useAccelerateEndpoint: true
 });
 
 module.exports.getObjectPromise = params => {
