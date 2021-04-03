2990dd8b8520c738b89bd0044f8b01ddc1b60bcd
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-04-03 19:36:38 +0800

    center aligned texts

--- src/functions/events/invoke/sendEmail.js
+++ src/functions/events/invoke/sendEmail.js
@@ -22,12 +22,12 @@ module.exports.handler = async ({
                   content +
                   `
                     <br>
-                    <p>
+                    <p style="text-align: center;">
                       <small>
                         This is a system generated email. Do not reply to this email.
                       </small>
                     </p>
-                    <p>
+                    <p style="text-align: center;">
                       <small>
                         If you need additional assistance, please send us an email to <a href="mailto:hello@entrepic.com">hello@entrepic.com</a>
                       </small>
