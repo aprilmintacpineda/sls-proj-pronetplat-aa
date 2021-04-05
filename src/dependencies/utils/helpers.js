ce755c031ab78978af9a0c9dcc6e6836b0ff347a
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-04-05 20:17:27 +0800

    small refactor

--- src/dependencies/utils/helpers.js
+++ src/dependencies/utils/helpers.js
@@ -43,6 +43,6 @@ module.exports.sanitizeFormBody = data => {
   }, {});
 };
 
-module.exports.wait = timeMs => {
-  return new Promise(resolve => setTimeout(resolve, timeMs));
+module.exports.sleep = seconds => {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
 };
