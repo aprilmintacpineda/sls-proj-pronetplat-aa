8e20ae0911e0e8f950c17611e8b8d362140408cc
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-02 21:28:30 +0800

    handle search by username

--- src/functions/auth/searchUsers.js
+++ src/functions/auth/searchUsers.js
@@ -1,7 +1,8 @@
 const { query } = require('faunadb');
 const {
   initClient,
-  isOnBlockList
+  isOnBlockList,
+  getByIndexIfExists
 } = require('dependencies/utils/faunadb');
 const {
   httpGuard,
@@ -9,54 +10,69 @@ const {
 } = require('dependencies/utils/guards');
 const { getPublicUserData } = require('dependencies/utils/users');
 
-async function handler ({ authUser, params: { search, nextToken } }) {
-  if (!search) return { statusCode: 400 };
+async function handler ({
+  authUser,
+  params: { search, searchBy, nextToken }
+}) {
+  if (!search || (searchBy !== 'name' && searchBy !== 'username'))
+    return { statusCode: 400 };
 
   const fauna = initClient();
+  let result = [];
 
-  const result = await fauna.query(
-    query.Filter(
-      query.Map(
-        query.Paginate(
-          query.Intersection(
-            query.Map(
-              query.NGram(search.toLowerCase(), 2, 3),
-              query.Lambda(
-                ['needle'],
-                query.Match(
-                  query.Index('searchUsersByName'),
-                  query.Var('needle')
+  if (searchBy === 'name') {
+    result = await fauna.query(
+      query.Filter(
+        query.Map(
+          query.Paginate(
+            query.Intersection(
+              query.Map(
+                query.NGram(search.toLowerCase(), 2, 3),
+                query.Lambda(
+                  ['needle'],
+                  query.Match(
+                    query.Index('searchUsersByName'),
+                    query.Var('needle')
+                  )
                 )
               )
-            )
+            ),
+            {
+              size: 20,
+              after: nextToken
+                ? query.Ref(query.Collection('contacts'), nextToken)
+                : []
+            }
           ),
-          {
-            size: 20,
-            after: nextToken
-              ? query.Ref(query.Collection('contacts'), nextToken)
-              : []
-          }
+          query.Lambda(['ref'], query.Get(query.Var('ref')))
         ),
-        query.Lambda(['ref'], query.Get(query.Var('ref')))
-      ),
-      query.Lambda(
-        ['user'],
-        query.And(
-          query.Not(
-            isOnBlockList(
-              authUser.id,
-              query.Select(['ref', 'id'], query.Var('user'))
+        query.Lambda(
+          ['user'],
+          query.And(
+            query.Not(
+              isOnBlockList(
+                authUser.id,
+                query.Select(['ref', 'id'], query.Var('user'))
+              )
+            ),
+            query.Select(
+              ['data', 'allowSearchByName'],
+              query.Var('user'),
+              false
             )
-          ),
-          query.Select(
-            ['data', 'allowSearchByName'],
-            query.Var('user'),
-            false
           )
         )
       )
-    )
-  );
+    );
+  } else {
+    result = await fauna.query(
+      getByIndexIfExists('userByUsername', search)
+    );
+
+    if (result.data.allowSearchByUsername)
+      result = result.data = [result];
+    else result = result.data = [];
+  }
 
   return {
     statusCode: 200,
