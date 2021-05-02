e5213d7dd2ada779a51d90e4417963d45dd25040
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-02 20:44:41 +0800

    change-allow-search-by-name endpoint

---
+++ src/functions/auth/changeAllowSearchByName.js
@@ -0,0 +1,42 @@
+const {
+  initClient,
+  updateById
+} = require('dependencies/utils/faunadb');
+const {
+  httpGuard,
+  guardTypes
+} = require('dependencies/utils/guards');
+const jwt = require('dependencies/utils/jwt');
+const { getUserData } = require('dependencies/utils/users');
+const validate = require('dependencies/utils/validate');
+
+async function handler ({
+  authUser,
+  formBody: { allowSearchByName }
+}) {
+  const client = initClient();
+
+  const user = await client.query(
+    updateById('users', authUser.id, { allowSearchByName })
+  );
+
+  const userData = getUserData(user);
+  const authToken = await jwt.sign(userData);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      userData,
+      authToken
+    })
+  };
+}
+
+module.exports.handler = httpGuard({
+  handler,
+  formValidator: {
+    allowSearchByName: ({ allowSearchByName }) =>
+      validate(allowSearchByName, ['required', 'bool'])
+  },
+  guards: [guardTypes.auth, guardTypes.setupComplete]
+});
