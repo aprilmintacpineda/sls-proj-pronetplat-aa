916b94400bd0b14932151abc29359a9f7a86a788
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-02 19:30:56 +0800

    used map then filter

--- src/functions/auth/searchUsers.js
+++ src/functions/auth/searchUsers.js
@@ -15,54 +15,45 @@ async function handler ({ authUser, params: { search, nextToken } }) {
   const fauna = initClient();
 
   const result = await fauna.query(
-    query.Reduce(
-      query.Lambda(
-        ['accumulator', 'ref'],
-        query.If(
-          isOnBlockList(
-            authUser.id,
-            query.Select(['id'], query.Var('ref'))
-          ),
-          query.Var('accumulator'),
-          query.Let(
-            {
-              user: query.Get(query.Var('ref'))
-            },
-            query.If(
-              query.Select(
-                ['data', 'allowSearchByName'],
-                query.Var('user'),
-                false
-              ),
-              query.Append(
-                query.Var('user'),
-                query.Var('accumulator')
-              ),
-              query.Var('accumulator')
-            )
-          )
-        )
-      ),
-      [],
-      query.Paginate(
-        query.Intersection(
-          query.Map(
-            query.NGram(search.toLowerCase(), 2, 3),
-            query.Lambda(
-              ['needle'],
-              query.Match(
-                query.Index('searchUsersByName'),
-                query.Var('needle')
+    query.Filter(
+      query.Map(
+        query.Paginate(
+          query.Intersection(
+            query.Map(
+              query.NGram(search.toLowerCase(), 2, 3),
+              query.Lambda(
+                ['needle'],
+                query.Match(
+                  query.Index('searchUsersByName'),
+                  query.Var('needle')
+                )
               )
             )
-          )
+          ),
+          {
+            size: 20,
+            after: nextToken
+              ? query.Ref(query.Collection('contacts'), nextToken)
+              : []
+          }
         ),
-        {
-          size: 20,
-          after: nextToken
-            ? query.Ref(query.Collection('contacts'), nextToken)
-            : []
-        }
+        query.Lambda(['var'], query.Get(query.Var('ref')))
+      ),
+      query.Lambda(
+        ['user'],
+        query.And(
+          query.Not(
+            isOnBlockList(
+              authUser.id,
+              query.Select(['id'], query.Var('ref'))
+            )
+          ),
+          query.Select(
+            ['data', 'allowSearchByName'],
+            query.Var('user'),
+            false
+          )
+        )
       )
     )
   );
@@ -72,10 +63,7 @@ async function handler ({ authUser, params: { search, nextToken } }) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      data: result.data.map(user => ({
-        ...getPublicUserData(user),
-        id: user.ref.id
-      })),
+      data: result.data.map(user => getPublicUserData(user)),
       nextToken: result.after?.[0].id || null
     })
   };
