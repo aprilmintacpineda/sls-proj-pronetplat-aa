42f9b9ab7ac99e177dbdb3241501943bbf2e0105
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-03 17:26:29 +0800

    sent-contact-requests endpoint

---
+++ src/functions/auth/sentContactRequests.js
@@ -0,0 +1,57 @@
+const { query } = require('faunadb');
+const { initClient } = require('dependencies/utils/faunadb');
+const {
+  httpGuard,
+  guardTypes
+} = require('dependencies/utils/guards');
+const { getPublicUserData } = require('dependencies/utils/users');
+
+async function handler ({ params: { nextToken }, authUser }) {
+  const client = initClient();
+
+  const result = await client.query(
+    query.Map(
+      query.Paginate(
+        query.Match(
+          query.Index('contactRequestsBySenderId'),
+          authUser.id
+        ),
+        {
+          size: 20,
+          after: nextToken
+            ? query.Ref(
+                query.Collection('contactRequests'),
+                nextToken
+              )
+            : []
+        }
+      ),
+      query.Lambda(['recipientId', 'ref'], {
+        contactRequest: query.Get(query.Var('ref')),
+        sender: query.Get(
+          query.Ref(
+            query.Collection('users'),
+            query.Var('recipientId')
+          )
+        )
+      })
+    )
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: result.data.map(({ contactRequest, sender }) => ({
+        ...contactRequest.data,
+        id: contactRequest.ref.id,
+        sender: getPublicUserData(sender)
+      })),
+      nextToken: result.after?.[0].id || null
+    })
+  };
+}
+
+module.exports.handler = httpGuard({
+  handler,
+  guards: [guardTypes.auth, guardTypes.setupComplete]
+});
