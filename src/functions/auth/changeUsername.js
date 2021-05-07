fe203f9ff316854886109893786f775e709f8fda
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-07 17:42:26 +0800

    respond with new token

--- src/functions/auth/changeUsername.js
+++ src/functions/auth/changeUsername.js
@@ -6,23 +6,34 @@ const {
   httpGuard,
   guardTypes
 } = require('dependencies/utils/guards');
+const jwt = require('dependencies/utils/jwt');
+const { getUserData } = require('dependencies/utils/users');
 const validate = require('dependencies/utils/validate');
 
 async function handler ({ authUser, formBody }) {
   const fauna = initClient();
 
   try {
-    await fauna.query(
+    const user = await fauna.query(
       updateById('users', authUser.id, {
         username: formBody.username || null
       })
     );
+
+    const userData = getUserData(user);
+    const authToken = await jwt.sign(userData);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        userData,
+        authToken
+      })
+    };
   } catch (error) {
     console.log(error);
     return { statusCode: 409 };
   }
-
-  return { statusCode: 200 };
 }
 
 module.exports.handler = httpGuard({
