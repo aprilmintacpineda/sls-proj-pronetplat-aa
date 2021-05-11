0766f6f123971534a598a788b27208490b35e69d
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-11 20:08:53 +0800

    sendChatMessage action

--- src/functions/websocket/sendWebsocketMessage.js
+++ src/functions/websocket/sendWebsocketMessage.js
@@ -1,6 +1,38 @@
-async function handler (event) {
-  console.log(JSON.stringify(event, null, 2));
-  console.log('sendWebsocketMessage');
+const AWS = require('aws-sdk');
+const { initClient, create } = require('dependencies/utils/faunadb');
+
+async function handler (webSocketEvent) {
+  const { action, data } = JSON.parse(webSocketEvent.body);
+
+  if (action !== 'sendmessage' && data.action !== 'sendChatMessage')
+    return { statusCode: 403 };
+
+  const { recipientId, messageBody, messageId } = data;
+
+  const faunadb = initClient();
+  const socket = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint:
+      webSocketEvent.requestContext.domainName +
+      '/' +
+      webSocketEvent.requestContext.stage
+  });
+
+  const payload = await faunadb.query(
+    create('chatMessages', {
+      recipientId,
+      messageBody
+    })
+  );
+
+  await socket.postToConnection({
+    ConectionId: webSocketEvent.requestContext.connectionId,
+    data: {
+      payload,
+      messageId
+    }
+  });
+
   return { statusCode: 200 };
 }
 
