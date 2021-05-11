9baf55f5b1f5435f30d51b4fa46bbba20bdd61aa
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-11 19:05:33 +0800

    saving web socket connection ids

--- src/functions/websocket/onUserConnected.js
+++ src/functions/websocket/onUserConnected.js
@@ -1,6 +1,28 @@
-async function handler (event) {
-  console.log('onUserConnected');
-  console.log(JSON.stringify(event, null, 2));
+const { initClient, create } = require('dependencies/utils/faunadb');
+const jwt = require('dependencies/utils/jwt');
+
+async function handler (webSocketEvent) {
+  const authorization = (webSocketEvent.headers.Authorization || '')
+    .replace(/Bearer /gim, '')
+    .trim();
+
+  if (!authorization) {
+    console.log('Guard: auth failed');
+    return { statusCode: 401 };
+  }
+
+  let authUser = await jwt.verify(authorization);
+  authUser = authUser.data;
+
+  const faunadb = initClient();
+
+  await faunadb.query(
+    create('userWebSocketConnections', {
+      userId: authUser.id,
+      connectionId: webSocketEvent.requestContext.connectionId
+    })
+  );
+
   return { statusCode: 200 };
 }
 
