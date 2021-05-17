dc06b37dd9b3abf9f756d3f551d0b469c758e356
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-17 22:04:35 +0800

    corrected grammar

--- src/functions/auth/sendChatMessage.js
+++ src/functions/auth/sendChatMessage.js
@@ -33,7 +33,7 @@ async function handler ({
     sendPushNotification({
       userId: contactId,
       title: 'New message from {fullname}',
-      body: '{fullname} send you a new chat message',
+      body: '{fullname} sent you a new chat message',
       authUser,
       data: {
         type: 'chatMessage',
