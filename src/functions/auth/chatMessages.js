1013c523e328630e2e4fc60224f1b7e8c97a8b88
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-20 17:18:31 +0800

    chat-messages/{contactId} route

---
+++ src/functions/auth/chatMessages.js
@@ -0,0 +1,58 @@
+const { query } = require('faunadb');
+const { initClient } = require('dependencies/utils/faunadb');
+const {
+  httpGuard,
+  guardTypes
+} = require('dependencies/utils/httpGuard');
+
+async function handler ({
+  authUser,
+  params: { contactId, nextToken }
+}) {
+  const faunadb = initClient();
+
+  const result = await faunadb.query(
+    query.Map(
+      query.Paginate(
+        query.Union(
+          query.Match(
+            query.Index('contactChatMessages'),
+            authUser.id,
+            contactId
+          ),
+          query.Match(
+            query.Index('contactChatMessages'),
+            contactId,
+            authUser.id
+          )
+        ),
+        {
+          size: 20,
+          after: nextToken
+            ? query.Ref(
+                query.Collection('contactChatMessages'),
+                nextToken
+              )
+            : []
+        }
+      ),
+      query.Lambda(['ref'], query.Get(query.Var('ref')))
+    )
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: result.data.map(document => ({
+        ...document.data,
+        id: document.ref.id
+      })),
+      nextToken: result.after?.[0].id || null
+    })
+  };
+}
+
+module.exports = httpGuard({
+  handler,
+  guards: [guardTypes.auth, guardTypes.setupComplete]
+});
