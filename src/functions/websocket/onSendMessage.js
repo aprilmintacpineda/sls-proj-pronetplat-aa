89250f61eaaa0c97eca4759def7b387dd3a7e3fb
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-30 14:33:46 +0800

    removed body from response

--- src/functions/websocket/onSendMessage.js
+++ src/functions/websocket/onSendMessage.js
@@ -1,10 +1,7 @@
 async function handler (websocketEvent) {
   console.log(websocketEvent);
 
-  return {
-    statusCode: 200,
-    body: 'pong'
-  };
+  return { statusCode: 200 };
 }
 
 module.exports.handler = handler;
