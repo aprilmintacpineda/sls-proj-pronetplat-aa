02698ad1540d1be5d54ddf466f6699b328c0d480
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-05-30 16:36:09 +0800

    handlers

--- src/functions/websocket/onSendMessage.js
+++ src/functions/websocket/onSendMessage.js
@@ -1,6 +1,16 @@
+const ping = require('dependencies/handlers/websocket/ping');
+
+const eventHandlers = {
+  ping
+};
+
 async function handler (webSocketEvent) {
-  console.log(webSocketEvent);
-  return { statusCode: 200 };
+  const { event, payload } = JSON.parse(webSocketEvent.body).data;
+
+  const handler = eventHandlers[event];
+  if (!handler) return { statusCode: 404 };
+
+  return eventHandlers(payload);
 }
 
 module.exports.handler = handler;
