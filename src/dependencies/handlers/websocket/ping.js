cd01b477cb2988b316c7a7f09a22765baceece75
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-06-06 18:13:23 +0800

    removed console log

--- src/dependencies/handlers/websocket/ping.js
+++ src/dependencies/handlers/websocket/ping.js
@@ -1,12 +1,11 @@
 const AWS = require('aws-sdk');
+
 const apiGateway = new AWS.ApiGatewayManagementApi({
   apiVersion: '2018-11-29',
   endpoint: '86iml7zhuc.execute-api.ap-southeast-1.amazonaws.com/dev'
 });
 
 module.exports = async webSocketEvent => {
-  console.log(JSON.stringify(webSocketEvent, null, 2));
-
   await apiGateway
     .postToConnection({
       ConnectionId: webSocketEvent.requestContext.connectionId,
