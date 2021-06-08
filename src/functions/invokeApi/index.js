b106b3e0fd6416a903a89054ca2e66bdc43378ce
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-06-08 20:46:22 +0800

    invokeEvent api

--- src/functions/invokeApi/index.js
+++ src/functions/invokeApi/index.js
@@ -1,6 +1,7 @@
 const events = require('./events');
 
 async function handler ({ event, payload }) {
+  console.log('---', event);
   const eventHandler = events[event];
   await eventHandler(payload);
 }
