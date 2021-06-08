29df13ec4fca08c272e81d43453a4f3bd5056882
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-06-08 23:14:46 +0800

    updated url

--- src/functions/webSocketApi/ping.js
+++ src/functions/webSocketApi/ping.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 const apiGateway = new AWS.ApiGatewayManagementApi({
   apiVersion: '2018-11-29',
-  endpoint: '86iml7zhuc.execute-api.ap-southeast-1.amazonaws.com/dev'
+  endpoint: 'ag8f8kbtz8.execute-api.ap-southeast-1.amazonaws.com/dev'
 });
 
 module.exports = async webSocketEvent => {
