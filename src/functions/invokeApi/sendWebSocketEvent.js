5d6a58fe9f91e011f506108e67784f5ec752d5c1
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-06-08 22:26:24 +0800

    updated websocket url

--- src/functions/invokeApi/sendWebSocketEvent.js
+++ src/functions/invokeApi/sendWebSocketEvent.js
@@ -8,7 +8,7 @@ const { getPublicUserData } = require('dependencies/utils/users');
 
 const apiGateway = new AWS.ApiGatewayManagementApi({
   apiVersion: '2018-11-29',
-  endpoint: '86iml7zhuc.execute-api.ap-southeast-1.amazonaws.com/dev'
+  endpoint: 'ag8f8kbtz8.execute-api.ap-southeast-1.amazonaws.com/dev'
 });
 
 module.exports = async ({ authUser, userId, type, payload }) => {
