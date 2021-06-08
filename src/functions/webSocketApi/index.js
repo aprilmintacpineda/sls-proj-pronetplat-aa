e5d54c892dfc1511e26f0d3441476ad307558cc7
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-06-08 22:02:13 +0800

    websocket api

--- src/functions/webSocketApi/index.js
+++ src/functions/webSocketApi/index.js
@@ -1,25 +1,34 @@
-const connect = require('./connect');
-const disconnect = require('./disconnect');
+const $connect = require('./connect');
+const $disconnect = require('./disconnect');
 const ping = require('./ping');
 
 const eventHandlers = {
-  ping,
-  $connect: connect,
-  $disconnect: disconnect
+  ping
 };
 
-async function handler (webSocketEvent) {
-  console.log(JSON.stringify(webSocketEvent));
-
+async function $default (webSocketEvent) {
   const { action, data } = JSON.parse(webSocketEvent.body);
 
-  const handler = eventHandlers[action];
-  if (!handler) {
+  const eventHandler = eventHandlers[action];
+  if (!eventHandler) {
     console.log('handler for action', action, 'not found');
     return { statusCode: 404 };
   }
 
-  return handler(webSocketEvent, data);
+  await eventHandler(data, webSocketEvent);
+}
+
+const routeKeyHandlers = {
+  $default: $default,
+  $connect: $connect,
+  $disconnect: $disconnect
+};
+
+async function handler (webSocketEvent) {
+  console.log(JSON.stringify(webSocketEvent));
+  const routeKey = webSocketEvent.requestContext.routeKey;
+  const routeKeyHandler = routeKeyHandlers[routeKey];
+  await routeKeyHandler(webSocketEvent);
 }
 
 module.exports.handler = handler;
