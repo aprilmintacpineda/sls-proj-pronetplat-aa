25b4dc9dd918239677d9a657dd528a873c09a1b0
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-07-12 19:09:31 +0800

    wip create event

--- src/functions/httpApi/auth/createEvent.js
+++ src/functions/httpApi/auth/createEvent.js
@@ -1,3 +1,9 @@
+const { query } = require('faunadb');
+const {
+  initClient,
+  existsByIndex,
+  create
+} = require('dependencies/utils/faunadb');
 const {
   httpGuard,
   guardTypes
@@ -5,11 +11,97 @@ const {
 const validate = require('dependencies/utils/validate');
 
 async function handler ({ authUser, formBody }) {
-  console.log(JSON.stringify({ authUser, formBody }));
+  // @todo validate location using place id?
 
-  return {
-    statusCode: 400
-  };
+  const faunadb = initClient();
+
+  let latitude = null;
+  let longitude = null;
+  let googlePlaceId = null;
+  let address = null;
+  let placeName = null;
+
+  try {
+    // resolve variables from google places
+    const location = formBody.location;
+    latitude = location.location.latitude;
+    longitude = location.location.longitude;
+    googlePlaceId = location.placeID;
+    address = location.address;
+    placeName = location.name;
+
+    if (
+      !latitude ||
+      !longitude ||
+      !googlePlaceId ||
+      !address ||
+      !placeName
+    )
+      throw new Error('A required location variable was not found.');
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400 };
+  }
+
+  try {
+    const createQuery = query.Let(
+      {
+        event: create('events', {
+          name: formBody.name,
+          description: formBody.description,
+          startDateTime: formBody.startDateTime,
+          endDateTime: formBody.endDateTime,
+          visibility: formBody.visibility,
+          maxAttendees: formBody.maxAttendees,
+          latitude,
+          longitude,
+          googlePlaceId,
+          address,
+          placeName
+        }),
+        eventId: query.Select(['ref', 'id'], query.Var('event'))
+      },
+      query.Do(
+        create('eventOrganizer', {
+          eventId: query.Var('eventId'),
+          userId: authUser.id
+        }),
+        ...formBody.organizers.map(user =>
+          create('eventOrganizer', {
+            eventId: query.Var('eventId'),
+            userId: user.id
+          })
+        )
+      )
+    );
+
+    // if organizer was set,
+    // validate that selected organizers are contacts of authUser
+    await faunadb.query(
+      formBody.organizers.length
+        ? query.If(
+            query.And(
+              formBody.organizers.map(contact =>
+                existsByIndex(
+                  'contactByOwnerContact',
+                  contact.id,
+                  authUser.id
+                )
+              )
+            ),
+            createQuery,
+            query.Abort('NonContactOrganizer')
+          )
+        : createQuery
+    );
+  } catch (error) {
+    if (error.description === 'NonContactOrganizer')
+      return { statusCode: 400 };
+
+    return { statusCode: 500 };
+  }
+
+  return { statusCode: 200 };
 }
 
 module.exports = httpGuard({
