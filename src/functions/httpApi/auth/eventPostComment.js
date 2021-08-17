e94473cc98a46c4e2f9bb876c594b2e3a775144b
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-08-17 19:55:32 +0800

    fixed organizers loop

--- src/functions/httpApi/auth/eventPostComment.js
+++ src/functions/httpApi/auth/eventPostComment.js
@@ -74,10 +74,8 @@ async function handler ({ authUser, formBody, params: { eventId } }) {
     return { statusCode: 500 };
   }
 
-  console.log(result);
-
   await Promise.all(
-    result.organizers.map(userId => {
+    result.organizers.data.map(userId => {
       if (userId === authUser.id) return null;
 
       return createNotification({
