36e622af2ce7c579a5834b75a67baff44c620942
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-08-22 13:03:44 +0800

    should not be able to reply to a reply;

--- src/functions/httpApi/auth/eventCommentReply.js
+++ src/functions/httpApi/auth/eventCommentReply.js
@@ -24,51 +24,64 @@ async function handler ({
 
   try {
     result = await faunadb.query(
-      query.Let(
-        {
-          comment: updateById('eventComments', commentId, {
-            numReplies: query.Add(
-              query.Select(
-                ['data', 'numReplies'],
-                getById('eventComments', commentId),
-                0
-              ),
-              1
-            )
-          }),
-          userId: query.Select(
-            ['data', 'userId'],
-            query.Var('comment')
-          ),
-          eventId: query.Select(
-            ['data', 'eventId'],
-            query.Var('comment')
+      query.If(
+        query.Not(
+          query.ContainsPath(
+            ['data', 'commentId'],
+            getById('eventComments', commentId)
           )
-        },
-        {
-          eventId: query.Var('eventId'),
-          userId: query.Var('userId'),
-          reply: create('eventComments', {
-            userId: authUser.id,
-            eventId: query.Var('eventId'),
-            comment: formBody.comment,
-            commentId
-          }),
-          organizers: query.Map(
-            query.Paginate(
-              query.Match(
-                query.Index('eventOrganizersByEvent'),
-                query.Var('eventId')
-              ),
-              { size: 20 }
+        ),
+        query.Let(
+          {
+            comment: updateById('eventComments', commentId, {
+              numReplies: query.Add(
+                query.Select(
+                  ['data', 'numReplies'],
+                  getById('eventComments', commentId),
+                  0
+                ),
+                1
+              )
+            }),
+            userId: query.Select(
+              ['data', 'userId'],
+              query.Var('comment')
             ),
-            query.Lambda(['userId', 'ref'], query.Var('userId'))
-          )
-        }
+            eventId: query.Select(
+              ['data', 'eventId'],
+              query.Var('comment')
+            )
+          },
+          {
+            eventId: query.Var('eventId'),
+            userId: query.Var('userId'),
+            reply: create('eventComments', {
+              userId: authUser.id,
+              eventId: query.Var('eventId'),
+              comment: formBody.comment,
+              commentId
+            }),
+            organizers: query.Map(
+              query.Paginate(
+                query.Match(
+                  query.Index('eventOrganizersByEvent'),
+                  query.Var('eventId')
+                ),
+                { size: 20 }
+              ),
+              query.Lambda(['userId', 'ref'], query.Var('userId'))
+            )
+          }
+        ),
+        query.Abort('ValidationError')
       )
     );
   } catch (error) {
     console.log('error', error);
+
+    if (error.description === 'ValidationError')
+      return { statusCode: 400 };
+
     return { statusCode: 500 };
   }
 
