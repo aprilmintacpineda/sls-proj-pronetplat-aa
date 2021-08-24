6bce7b3ebea68f84bc69db74564f3f730f1b9dee
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-08-24 16:21:19 +0800

    my events past present future

--- src/functions/httpApi/auth/myEvents.js
+++ src/functions/httpApi/auth/myEvents.js
@@ -9,48 +9,149 @@ const {
   guardTypes
 } = require('dependencies/utils/httpGuard');
 
-async function handler ({ params: { nextToken }, authUser }) {
+async function handler ({
+  params: { nextToken, schedule },
+  authUser
+}) {
   const faunadb = initClient();
   const nextTokenParts = nextToken ? nextToken.split('___') : null;
+  let result = null;
 
-  const result = await faunadb.query(
-    query.Map(
-      query.Paginate(
-        query.Union(
-          query.Match(
-            query.Index('eventsByEventOrganizer'),
-            authUser.id
-          ),
-          query.Match(query.Index('eventsByAttendee'), authUser.id)
-        ),
-        {
-          size: 20,
-          after: nextTokenParts
-            ? [
-                nextTokenParts[0],
-                query.Ref(
-                  query.Collection('eventOrganizers'),
-                  nextTokenParts[1]
-                )
-              ]
-            : []
-        }
-      ),
-      query.Lambda(['eventId', 'ref'], {
-        event: getById('_events', query.Var('eventId')),
-        isGoing: existsByIndex(
-          'eventAttendeeByUserEvent',
-          authUser.id,
-          query.Var('eventId')
-        ),
-        isOrganizer: existsByIndex(
-          'eventOrganizerByOrganizerEvent',
-          authUser.id,
-          query.Var('eventId')
-        )
-      })
+  if (schedule) {
+    if (
+      schedule !== 'future' &&
+      schedule !== 'past' &&
+      schedule !== 'present'
     )
-  );
+      return { statusCode: 400 };
+
+    result = await faunadb.query(
+      query.Filter(
+        query.Map(
+          query.Paginate(
+            query.Union(
+              query.Match(
+                query.Index('eventsByEventOrganizer'),
+                authUser.id
+              ),
+              query.Match(
+                query.Index('eventsByAttendee'),
+                authUser.id
+              )
+            ),
+            {
+              size: 20,
+              after: nextTokenParts
+                ? [
+                    nextTokenParts[0],
+                    query.Ref(
+                      query.Collection('eventOrganizers'),
+                      nextTokenParts[1]
+                    )
+                  ]
+                : []
+            }
+          ),
+          query.Lambda(['eventId', 'ref'], {
+            event: getById('_events', query.Var('eventId')),
+            isGoing: existsByIndex(
+              'eventAttendeeByUserEvent',
+              authUser.id,
+              query.Var('eventId')
+            ),
+            isOrganizer: existsByIndex(
+              'eventOrganizerByOrganizerEvent',
+              authUser.id,
+              query.Var('eventId')
+            )
+          })
+        ),
+        query.Lambda(
+          ['document'],
+          schedule === 'past'
+            ? query.LT(
+                query.TimeDiff(
+                  query.Now(),
+                  query.Time(
+                    query.Select(
+                      ['event', 'data', 'startDateTime'],
+                      query.Var('document')
+                    )
+                  ),
+                  'days'
+                ),
+                0
+              )
+            : schedule === 'future'
+            ? query.GT(
+                query.TimeDiff(
+                  query.Now(),
+                  query.Time(
+                    query.Select(
+                      ['event', 'data', 'startDateTime'],
+                      query.Var('document')
+                    )
+                  ),
+                  'days'
+                ),
+                0
+              )
+            : query.Equals(
+                query.TimeDiff(
+                  query.Now(),
+                  query.Time(
+                    query.Select(
+                      ['event', 'data', 'startDateTime'],
+                      query.Var('document')
+                    )
+                  ),
+                  'days'
+                ),
+                0
+              )
+        )
+      )
+    );
+  } else {
+    result = await faunadb.query(
+      query.Map(
+        query.Paginate(
+          query.Union(
+            query.Match(
+              query.Index('eventsByEventOrganizer'),
+              authUser.id
+            ),
+            query.Match(query.Index('eventsByAttendee'), authUser.id)
+          ),
+          {
+            size: 20,
+            after: nextTokenParts
+              ? [
+                  nextTokenParts[0],
+                  query.Ref(
+                    query.Collection('eventOrganizers'),
+                    nextTokenParts[1]
+                  )
+                ]
+              : []
+          }
+        ),
+        query.Lambda(['eventId', 'ref'], {
+          event: getById('_events', query.Var('eventId')),
+          isGoing: existsByIndex(
+            'eventAttendeeByUserEvent',
+            authUser.id,
+            query.Var('eventId')
+          ),
+          isOrganizer: existsByIndex(
+            'eventOrganizerByOrganizerEvent',
+            authUser.id,
+            query.Var('eventId')
+          )
+        })
+      )
+    );
+  }
 
   return {
     statusCode: 200,
