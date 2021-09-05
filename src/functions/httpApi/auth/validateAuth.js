6056b51940b554adb9bca71d1f6d6804b1c2428a
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-09-05 09:03:28 +0800

    reverted back

--- src/functions/httpApi/auth/validateAuth.js
+++ src/functions/httpApi/auth/validateAuth.js
@@ -1,51 +1,49 @@
-// const { query } = require('faunadb');
-// const {
-//   initClient,
-//   createOrUpdate,
-//   updateById
-// } = require('dependencies/utils/faunadb');
+const { query } = require('faunadb');
+const {
+  initClient,
+  createOrUpdate,
+  updateById
+} = require('dependencies/utils/faunadb');
 const {
   httpGuard,
   guardTypes
 } = require('dependencies/utils/httpGuard');
-// const jwt = require('dependencies/utils/jwt');
-// const { getUserData } = require('dependencies/utils/users');
+const jwt = require('dependencies/utils/jwt');
+const { getUserData } = require('dependencies/utils/users');
 
-async function handler (/**{ authUser, deviceToken } */) {
-  return { statusCode: 400 };
+async function handler ({ authUser, deviceToken }) {
+  const faunadb = initClient();
 
-  // const faunadb = initClient();
+  const user = await faunadb.query(
+    query.Do(
+      createOrUpdate({
+        collection: 'registeredDevices',
+        index: 'registeredDeviceByUserIdDeviceToken',
+        args: [authUser.id, deviceToken],
+        data: {
+          userId: authUser.id,
+          deviceToken
+        }
+      }),
+      updateById('users', authUser.id, {
+        lastLoginAt: query.Format(
+          '%t',
+          query.TimeAdd(query.Now(), 7, 'days')
+        )
+      })
+    )
+  );
 
-  // const user = await faunadb.query(
-  //   query.Do(
-  //     createOrUpdate({
-  //       collection: 'registeredDevices',
-  //       index: 'registeredDeviceByUserIdDeviceToken',
-  //       args: [authUser.id, deviceToken],
-  //       data: {
-  //         userId: authUser.id,
-  //         deviceToken
-  //       }
-  //     }),
-  //     updateById('users', authUser.id, {
-  //       lastLoginAt: query.Format(
-  //         '%t',
-  //         query.TimeAdd(query.Now(), 7, 'days')
-  //       )
-  //     })
-  //   )
-  // );
+  const userData = getUserData(user);
+  const authToken = await jwt.sign(userData);
 
-  // const userData = getUserData(user);
-  // const authToken = await jwt.sign(userData);
-
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     authToken,
-  //     userData
-  //   })
-  // };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      authToken,
+      userData
+    })
+  };
 }
 
 module.exports = httpGuard({
