3a8e3e8973e138b2b654d4ed5fde88055f4f34d4
April Mintac Pineda <aprilmintacpineda@gmail.com>
2021-09-10 22:38:13 +0800

    used Reduce

--- src/functions/httpApi/auth/myEvents.js
+++ src/functions/httpApi/auth/myEvents.js
@@ -17,101 +17,105 @@ async function handler ({
   const nextTokenParts = nextToken ? nextToken.split('___') : null;
 
   const result = await faunadb.query(
-    query.Map(
-      query.Filter(
-        query.Paginate(
-          query.Join(
-            query.Union(
-              query.Match(
-                query.Index('eventsByEventOrganizer'),
-                authUser.id
-              ),
-              query.Match(
-                query.Index('eventsByAttendee'),
-                authUser.id
-              )
-            ),
-            query.Lambda(
-              ['eventId', 'ref'],
-              query.Match(
-                query.Index('eventsSortedByDateTime'),
-                query.Ref(
-                  query.Collection('_events'),
-                  query.Var('eventId')
-                )
-              )
-            )
-          ),
-          {
-            size: 20,
-            after: nextTokenParts
-              ? [
-                  nextTokenParts[0],
-                  nextTokenParts[1],
-                  query.Ref(
-                    query.Collection('eventOrganizers'),
-                    nextTokenParts[2]
-                  )
-                ]
-              : []
-          }
-        ),
-        query.Lambda(
-          ['startDateTime', 'endDateTime', 'ref'],
+    query.Reduce(
+      query.Lambda(
+        ['accumulator', 'values'], // ['startDateTime', 'endDateTime', 'ref']
+        query.If(
           schedule === 'past'
             ? query.And(
                 query.LT(
-                  query.Time(query.Var('startDateTime')),
+                  query.Time(query.Select([0], query.Var('values'))),
                   query.Now()
                 ),
                 query.LT(
-                  query.Time(query.Var('endDateTime')),
+                  query.Time(query.Select([1], query.Var('values'))),
                   query.Now()
                 )
               )
             : schedule === 'future'
             ? query.GT(
-                query.Time(query.Var('startDateTime')),
+                query.Time(query.Select([0], query.Var('values'))),
                 query.Now()
               )
             : query.And(
                 query.LTE(
-                  query.Time(query.Var('startDateTime')),
+                  query.Time(query.Select([0], query.Var('values'))),
                   query.Now()
                 ),
                 query.GTE(
-                  query.Time(query.Var('endDateTime')),
+                  query.Time(query.Select([1], query.Var('values'))),
                   query.Now()
                 )
+              ),
+          query.Append(
+            {
+              event: query.Get(query.Var('ref')),
+              isGoing: existsByIndex(
+                'eventAttendeeByUserEventStatus',
+                authUser.id,
+                query.Select(['id'], query.Var('ref')),
+                'active'
+              ),
+              isOrganizer: existsByIndex(
+                'eventOrganizerByOrganizerEvent',
+                authUser.id,
+                query.Select(['id'], query.Var('ref'))
               )
+            },
+            query.Var('accumulator')
+          ),
+          query.Var('accumulator')
         )
       ),
-      query.Lambda(['startDateTime', 'endDateTime', 'ref'], {
-        event: query.Get(query.Var('ref')),
-        isGoing: existsByIndex(
-          'eventAttendeeByUserEventStatus',
-          authUser.id,
-          query.Select(['id'], query.Var('ref')),
-          'active'
+      [],
+      query.Paginate(
+        query.Join(
+          query.Union(
+            query.Match(
+              query.Index('eventsByEventOrganizer'),
+              authUser.id
+            ),
+            query.Match(query.Index('eventsByAttendee'), authUser.id)
+          ),
+          query.Lambda(
+            ['eventId', 'ref'],
+            query.Match(
+              query.Index('eventsSortedByDateTime'),
+              query.Ref(
+                query.Collection('_events'),
+                query.Var('eventId')
+              )
+            )
+          )
         ),
-        isOrganizer: existsByIndex(
-          'eventOrganizerByOrganizerEvent',
-          authUser.id,
-          query.Select(['id'], query.Var('ref'))
-        )
-      })
+        {
+          size: 20,
+          after: nextTokenParts
+            ? [
+                nextTokenParts[0],
+                nextTokenParts[1],
+                query.Ref(
+                  query.Collection('eventOrganizers'),
+                  nextTokenParts[2]
+                )
+              ]
+            : []
+        }
+      )
     )
   );
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      data: result.data.map(({ event, isOrganizer, isGoing }) => ({
-        id: event.ref.id,
-        ...event.data,
-        isGoing,
-        isOrganizer
-      })),
+      data: result.data[0].map(
+        ({ event, isOrganizer, isGoing }) => ({
+          id: event.ref.id,
+          ...event.data,
+          isGoing,
+          isOrganizer
+        })
+      ),
       nextToken: result.after
         ? `${result.after[0]}___${result.after[1]}___${result.after[2].id}`
         : null
